f9577e9233f6f26b2389fd1d8de26dd4158d81f6
Alex Nichol <unixpickle@gmail.com>
2014-06-30 17:34:04 -0400

    indent strlen.s

--- examples/strlen.s
+++ examples/strlen.s
@@ -9,14 +9,14 @@
 ; return value: r3
 ;
 .strlen
-sreg r3, 0
+  sreg r3, 0
 ._loop
-gmem r1, r5
-sreg r4, 0
-ucmp r5, r4
-je r0
-sreg r4, 1
-uadd r3, r3, r4
-uadd r1, r1, r4
-sreg r4, ._loop
-jmp r4
+  gmem r1, r5
+  sreg r4, 0
+  ucmp r5, r4
+  je r0
+  sreg r4, 1
+  uadd r3, r3, r4
+  uadd r1, r1, r4
+  sreg r4, ._loop
+  jmp r4
